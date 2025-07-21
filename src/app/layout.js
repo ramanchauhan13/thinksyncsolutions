36e29234e1b84c51ce74aed2cd35c78fead9cb67
Raman Chauhan <ramanchauhan13@gmail.com>
2025-07-21 15:36:06 +0530

    feat: Update description in metadata for clarity and relevance

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,12 @@
 import "./globals.css";
 export const metadata = {
   title: 'THINKSYNC SOLUTIONS',
-  description: 'Cutting-edge digital experiences',
+  description: 'We build custom software, ERP Portals, and modern web apps to accelerate your digital growth.',
   icons: {
     icon: '/favicon.ico',
   },
 }
+
 
 export default function RootLayout({ children }) {
   return (
